58973512735b54f8e7da7e130b8135d762afc3f3
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-08-08 18:47:57 +0530

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -23,11 +23,11 @@ routes = web.RouteTableDef()
 @routes.get("/", allow_head=True)
 async def root_route_handler(_):
     return web.json_response(
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3967423603573430"
+     crossorigin="anonymous"></script>
         {
             "server_status": "running",
             "uptime": get_readable_time(time.time() - StartTime),
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3967423603573430"
-     crossorigin="anonymous"></script>
             "telegram_bot": "@" + StreamBot.username,
             "connected_bots": len(multi_clients),
             "loads": dict(
